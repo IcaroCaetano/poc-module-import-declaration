ef46377c0b7e4e259ebd49e1296df98c80bb2010
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-15 22:33:57 -0300

    first commit

---
+++ src/main/java/com/project/poc_module_import_declaration/PocModuleImportDeclarationApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_module_import_declaration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocModuleImportDeclarationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocModuleImportDeclarationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_module_import_declaration/PocModuleImportDeclarationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_module_import_declaration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocModuleImportDeclarationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
